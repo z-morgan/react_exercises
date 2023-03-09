9cdb2f328305a38a21ddee1d582d77fb9dfa95a9
Zach Morgan <zachmorgan97@gmail.com>
2023-03-09 14:18:36 -0800

    connected front-end to back-end for developement

--- part2/phonebook/src/services/personsAPI.js
+++ part2/phonebook/src/services/personsAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const PATH = 'http://localhost:3001/persons';
+const PATH = '/api/persons';
 
 const getAll = () => {
   return axios.get(PATH).then(response => response.data);
