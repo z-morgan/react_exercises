1bd52815f315033792e53a4b06bb88f50a30fafd
Zach Morgan <zachmorgan97@gmail.com>
2023-03-10 12:31:56 -0800

    restructured app files for better scaling

--- part3/part3/models/note.js
+++ part3/part3/models/note.js
@@ -1,23 +1,11 @@
 const mongoose = require('mongoose')
-const { config } = require('dotenv');
-config();
-
-mongoose.set('strictQuery', false)
-
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
 
 const noteSchema = new mongoose.Schema({
-  content: String,
+  content: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
   important: Boolean,
 })
 
